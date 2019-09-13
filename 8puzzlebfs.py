47d97e1d3c3d9f86208b9fa11662b4612b594d94
Jarelio <jareliofilho@gmail.com>
2019-09-13 07:30:32 -0300

    Adição de comentários

--- 8puzzlebfs.py
+++ 8puzzlebfs.py
@@ -3,17 +3,23 @@ import time
 import os
 
 class No:
+    #Construtor da classe No, possui cor (usar no algoritmo bfs), 
+    #lista de adjacencia (nós estados), valor (estado), pai(guarda o no do qual ele veio)
     def __init__(self,value,pai,adj=[],cor='branco'):
         self.cor = cor
         self.adj = adj
         self.value = value
         self.pai = pai
 
+    #funcao para inserir um no na lista da adjacencia
     def __inseriradj__(self,valor):
         self.adj.append(No(valor,self))
 
+    #funcao para criar lista de adjacencia (ao visitar um estado, ela eh chamada para criar os estados filhos)
     def crialistaadj(self):
-
+        #estado new_value vai receber o estado referente ao estado anterior movendo o 0 para esquerda
+        #caso ele seja diferente do valor atual do estado (ocorra uma mudanca)
+        #o mesmo pros outros casos
         new_value = self.__movesq__()
         if(new_value != self.value):
             self.__inseriradj__(new_value)
@@ -21,8 +27,8 @@ class No:
         new_value = self.__movdir__()
         if(new_value != self.value):
             self.__inseriradj__(new_value)
-        new_value = self.__movcim__()
 
+        new_value = self.__movcim__()
         if(new_value != self.value):
             self.__inseriradj__(new_value)
 
@@ -30,6 +36,7 @@ class No:
         if(new_value != self.value):
             self.__inseriradj__(new_value)
 
+    #mover para direcao, primeiro encontra a peca 0 e cria um novo estado com ela na posicao movida
     def __movesq__(self):
         linha,coluna = self.__encontrarpeca0__()
         new_value = copy.deepcopy(self.value)
@@ -58,6 +65,7 @@ class No:
             new_value[linha+1][coluna],new_value[linha][coluna] = self.value[linha][coluna],self.value[linha+1][coluna] 
         return new_value
     
+    #funcao para encontrar a peca 0 na matriz 3x3
     def __encontrarpeca0__(self):
         for i in range(3):
             for j in range(3):
@@ -66,16 +74,19 @@ class No:
                     coluna = j
                     return linha,coluna
 
+#fila FIFO para ser usada no algoritmo bfs
 class Fila:
     def __init__(self,raiz,lista = []):
         self.lista = lista
         self.lista.append(raiz)
 
+    #remove o primeiro elemento da fila (lista)
     def removefila(self):
         no = self.lista[0]
         self.lista.remove(no)
         return no
 
+    #insere no final da fila (lista)
     def inserefila(self,no):
         self.lista.append(no)
 
@@ -85,6 +96,9 @@ class Fila:
     def filavazia(self):
         return len(self.lista)==0
 
+#algoritmo bfs do cormen, com a modificacao nas linhas 108 a 112
+#modificacao: ao visitar um no, verificar se ele eh o estado meta, se for,
+#montar a lista_solucao que eh os estados do no meta ate a raiz (percorrendo os pais)
 def bfs(r,meta):
     lista_solucao = []
     r.cor = 'cinza'
@@ -95,24 +109,28 @@ def bfs(r,meta):
             while(u != None):
                 lista_solucao.append(u)
                 u = u.pai
-            break
+            return lista_solucao
+        #quando um no eh visitado (retirado da fila) cria-se a lista de nos filhos dele (adjacencia / proximos estados)
         u.crialistaadj()
         for v in u.adj:
             if(v.cor == 'branco'):
                 v.cor = 'cinza'
                 fila.inserefila(v)
         u.cor = 'preto'
-    return lista_solucao
-
-def main():
-
-    meta = [[1,2,3],[4,5,6],[7,8,0]]
-    no_inicial = No([[4,1,3],[7,2,5],[0,8,6]],None)
-    lista_solucao = bfs(no_inicial,meta)
-    #solucao
-    lista_solucao.reverse()
     
 
+def main():
+    #cria o no meta pra ser testado
+    meta = [[1,2,3],[4,5,6],[7,8,0]]
+
+    #cria o no raiz (inicial) passando o estado e None (valor do pai da raiz)
+    no_inicial = No([[4,1,3],[7,2,5],[0,8,6]],None)
+
+    lista_solucao = bfs(no_inicial,meta)
+    
+    #lista solucao eh o caminho do valor da raiz ate o valor do estado folha
+    lista_solucao.reverse()
+    
     #Print animado da solução
     os.system("clear")
     print("Solucionar: ")
